8282a5a727a8285678917fd4e81244da1d402cc5
Ricardo Mejias Travieso <63120247+Ricardo-MT@users.noreply.github.com>
2023-08-12 16:12:56 +0200

    Add players
    Create players animations, add level 2, place player in spawn points

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -1,14 +1,32 @@
 import 'dart:async';
 
+import 'package:adventure/actors/player.dart';
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 
 class Level extends World {
+  Level({
+    required this.levelName,
+  });
+  late String levelName;
   late TiledComponent level;
   @override
   FutureOr<void> onLoad() async {
-    level = await TiledComponent.load('Level-1.tmx', Vector2.all(16));
+    level = await TiledComponent.load('$levelName.tmx', Vector2.all(16));
     add(level);
+    final spawPoints = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
+    for (var point in (spawPoints!.objects)) {
+      switch (point.class_) {
+        case 'Player':
+          final player = Player(
+            character: PlayerCharacter.maskDude,
+            position: Vector2(point.x, point.y),
+          );
+          add(player);
+          break;
+        default:
+      }
+    }
     return super.onLoad();
   }
 }
